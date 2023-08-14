c0e491dcee70120517f6e626da8723b41c24b346
Kevin Kyaterekera <kn.kyaterekera@gmail.com>
2023-08-14 11:34:14 +0200

    add: file and data-structure

---
+++ components/lib/data.js
@@ -0,0 +1,9 @@
+export const travel = [
+  {
+    name: "VC Pines Concert",
+    destination: "London",
+    timePeriod: "13.09.2023 - 15.09.2023",
+  },
+  { id: "0", participant: "User 1" },
+  { id: "1", participant: "User 2" },
+];
