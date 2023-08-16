94c3afe19e52e8f7446d846d84870c47bad83342
Kevin Kyaterekera <kn.kyaterekera@gmail.com>
2023-08-16 17:28:11 +0200

    fixed Link component - redirects now to overview

--- components/Button.js
+++ components/Button.js
@@ -3,16 +3,22 @@ import { styled } from "styled-components";
 
 export default function Button() {
   return (
-    <Link href="/">
-      <StyledButton>Back to overview</StyledButton>
-    </Link>
+    <StyledButton>
+      <StyledButtonLink href="/">Back to overview</StyledButtonLink>
+    </StyledButton>
   );
 }
 
 const StyledButton = styled.button`
   border: 0.5px solid black;
   font-size: 1em;
-  border-radius: 0.5rem;
+  border-radius: 0.45rem;
   padding: 0.25em 1em;
   margin: 1em;
+`;
+
+const StyledButtonLink = styled(Link)`
+  width: 100%;
+  text-decoration: none;
+  color: black;
 `;
