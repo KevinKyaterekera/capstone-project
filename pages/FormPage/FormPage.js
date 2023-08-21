023ab9e8f67607d55bb581a4e6534959bee3a083
Kevin Kyaterekera <kn.kyaterekera@gmail.com>
2023-08-21 12:02:07 +0200

    added CreateTripButton and CreateTripForm

---
+++ pages/FormPage/FormPage.js
@@ -0,0 +1,18 @@
+import Button from "@/components/Button";
+import { styled } from "styled-components";
+
+export default function CreateTripForm() {
+  return (
+    <FlexContainer>
+      <h2>Create your trip</h2>
+      <Button />
+    </FlexContainer>
+  );
+}
+
+const FlexContainer = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.6rem;
+`;
